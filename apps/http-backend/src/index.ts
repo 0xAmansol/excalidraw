b2f38f456e6dca9b8fb4c7a2167081caab1e0e95
Aman kalal <amankalal206@gmail.com>
2025-01-18 18:19:51 +0530

    completed http layer

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,40 +1,96 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { middleware } from "./middleware";
+import { middleware } from "./middleware.js";
+import { roomSchema, signUpSchema, userSchema } from "@workspace/common/types";
 import { JWT_SECRET } from "@workspace/backend-common/config";
-import { userSchema } from "@workspace/common/types";
 import { prisma } from "@workspace/db/client";
 
 const app = express();
+const port = 3001;
+
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = userSchema.safeParse(req.body);
-  if (user.success) {
-    await prisma.user.create({
+  const parsedUser = signUpSchema.safeParse(req.body);
+  if (!parsedUser.success) {
+    res.status(403).json({
+      message: "Invalid Inputs",
+    });
+    return;
+  }
+  try {
+    const user = await prisma.user.create({
       data: {
-        username: user.data.username,
-        password: user.data.password,
+        email: parsedUser.data.email,
+        password: parsedUser.data.password,
+        name: parsedUser.data.username,
       },
     });
-    res.json({ success: true });
-  } else {
     res.json({
-      message: "Invalid Inputs",
+      message: `user created with userId ${user.id}`,
+    });
+  } catch (error) {
+    res.status(403).json({
+      message: "email already exists",
     });
   }
 });
 
-app.post("/signin", (req, res) => {
-  const userId = 1;
+app.post("/signin", async (req, res) => {
+  const parsedUser = userSchema.safeParse(req.body);
+
+  if (!parsedUser.success) {
+    res.json({
+      message: "Incorrect Inputs",
+    });
+    return;
+  }
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email: parsedUser.data?.email,
+      password: parsedUser.data.password,
+    },
+  });
+
+  if (!user) {
+    res.status(403).json({
+      message: "user unauthorized",
+    });
+  }
+
+  const userId = user?.id;
   const token = jwt.sign({ userId }, JWT_SECRET);
 
   res.json({ token });
 });
 
-app.post("/room", middleware, (req, res) => {
-  res.json({ userId: req.userId });
+app.post("/room", middleware, async (req, res) => {
+  const parsedUser = roomSchema.safeParse(req.body);
+  if (!parsedUser.success) {
+    res.json({
+      message: "Invalid room name",
+    });
+    return;
+  }
+
+  const userId = req.userId;
+
+  try {
+    const room = await prisma.room.create({
+      data: {
+        slug: parsedUser.data.name,
+        adminId: userId,
+      },
+    });
+    res.json({ roomId: room.id });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error creating room",
+    });
+  }
 });
 
-const port = 3001;
-
-app.listen(port);
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const userSchema = z.object({
   username: z.string(),
   password: z.string(),
+  email: z.string(),
 });
 
 export const roomSchema = z.object({
@@ -10,6 +11,7 @@ export const roomSchema = z.object({
 });
 
 export const signUpSchema = z.object({
-  username: z.string(),
-  password: z.string(),
+  email: z.string().min(1),
+  password: z.string().min(1),
+  username: z.string().min(1),
 });
